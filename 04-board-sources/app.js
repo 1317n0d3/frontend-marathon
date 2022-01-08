5e60e1f77bcd1da9cd7059f58055535053f0cf47
klltx <kostya.svetashov.01@gmail.com>
2022-01-08 13:16:57 +0400

    init new project

---
+++ 04-board-sources/app.js
@@ -0,0 +1,39 @@
+const board = document.querySelector("#board");
+const colors = [
+  "#141f1f",
+  "#cfc52e",
+  "#4ecf2e",
+  "#2ecfac",
+  "#2e59cf",
+  "#cf2ec2",
+  "#cf2e51",
+  "#e6d88b",
+];
+const SQUARES_NUMBER = 1000;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
